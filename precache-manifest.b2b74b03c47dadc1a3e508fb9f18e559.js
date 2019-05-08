83799f94718bd2d191ced4bd1c01d37e31ef6181
Artem Valkov <valkov.artyom@gmail.com>
2019-05-08 06:20:16 +0300

    Updates

---
+++ precache-manifest.b2b74b03c47dadc1a3e508fb9f18e559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d6ea778f58a23793e7355df8f0bddd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5c64cf5f06e65eab0325",
+    "url": "/portfolio/static/css/main.039b3658.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5c64cf5f06e65eab0325",
+    "url": "/portfolio/static/js/main.99a5cbaf.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ddc2dbf77f665d35e091d2cff37ac3e9",
+    "url": "/portfolio/static/media/background.ddc2dbf7.jpg"
+  }
+]);
