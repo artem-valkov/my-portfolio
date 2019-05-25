bbf8e667d7ed57d0ce968b79ddc13a7ec3409da1
Artem Valkov <valkov.artyom@gmail.com>
2019-05-25 17:46:56 +0300

    react-router fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ export default class Main extends React.Component {
     return (
       <main>
         <Switch>
-          <Route path='/home' component={Header} />
+          <Route exact path='/' component={Header} /> } />
           <Route path='/about' component={About} />
           <Route path='/projects' component={Projects} />
           <Route path='/contacts' component={Contacts} />
